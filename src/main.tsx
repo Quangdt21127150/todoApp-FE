4be931dcd8906abdb7ab35a0c1f9efc78b23bdee
Quang <nghnquang2003@gmail.com>
2024-12-17 21:49:17 +0700

    Third commit

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,14 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { Provider } from "react-redux";
-import { store } from "./store/store.tsx";
-import App from "./App";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 import "./index.css";
+import App from "./App.tsx";
+import { store } from "./store/store.tsx";
+import { Provider } from "react-redux";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
     <Provider store={store}>
       <App />
     </Provider>
-  </React.StrictMode>
+  </StrictMode>
 );
